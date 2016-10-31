84fa28034bb4241d075bc70c64916e9d219ed8b4
Guilherme Matuella <guilherme1matuella@gmail.com>
2016-10-31 00:44:58 -0200

    Views (templates) adaptation. Few tweaks left (search and scope errors)

--- src/js/controllers/roteiro-controller.js
+++ src/js/controllers/roteiro-controller.js
@@ -22,12 +22,19 @@ function RoteiroController($scope, RoteiroService, CursoService) {
     $scope.addRoteiro = function(newRoteiro){
         jsogRoteiro = JSOG.encode(newRoteiro);
 
+
+        console.log($scope.pesquisaRoteiro);
+        console.log($scope.roteiro);
+        console.log(jsogRoteiro);
+
         RoteiroService.saveRoteiro(jsogRoteiro).then(function (response) {
             if(response.status == 200){
                 $scope.roteiros.push(JSOG.decode(response.data)); //Isso não vai dar problema de inconsistência por n ter o id?
             } else{
                 //Tratar erro com pop-up na tela.
             }
+
+            console.log("DELETANDO: " + $scope.roteiro);
             delete $scope.roteiro;
         });
     }
