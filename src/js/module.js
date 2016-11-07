036a438714ca42d18fafd9947a89d1b189886ae8
Guilherme Matuella <guilherme1matuella@gmail.com>
2016-11-06 22:18:38 -0200

    Changing the config rootUrl value to the Heroku one

--- src/js/module.js
+++ src/js/module.js
@@ -1,6 +1,6 @@
 var app = angular.module('Gastromatic', ['ui.bootstrap', 'ui.router', 'ngCookies']);
 
 app.value("config", {
-    rootUrl: "http://localhost:8080/gastromatic"
-    // rootUrl: "https://gastromatic.herokuapp.com"
+    // rootUrl: "http://localhost:8080/gastromatic"
+    rootUrl: "https://gastromatic.herokuapp.com"
 });
